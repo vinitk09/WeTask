4c53d21c801ca3fc2b9887ae6a4ddb250383523d
vinitk09 <vinitkumar8092@gmail.com>
2024-12-10 17:05:50 +0530

    First

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  webpack: (config, { isServer }) => {
-    // Example: Adding a custom loader for .md files
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader", // or any other loader you want to add
-    });
-
-    // Example: Add fallback for `fs` module (used in browser-side code)
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    // You can add more custom configurations here.
-
-    return config;
+export default {
+  images: {
+    unoptimized: true,
   },
 };
-
-export default nextConfig;
+// export default nextConfig;
